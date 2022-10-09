2d632fa2e66414636e037275d304e7d97de6a903
OmkarRode <rodeomkar5@gmail.com>
2022-10-10 00:32:39 +0530

    set border color

--- DOM_Task3/javascript.js
+++ DOM_Task3/javascript.js
@@ -1,1 +1,12 @@
+
+//task 3 first activity
 document.getElementById("header-title").innerText="hello Sharpner";
+
+// task 3 second activity . set border color 
+
+var title=document.getElementById("header-title");
+title.style.border="2px solid black";
+
+
+
+
