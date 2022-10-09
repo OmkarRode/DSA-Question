d31e9edd4d399e9044afbc6dfc5c5324ab02cd7f
OmkarRode <rodeomkar5@gmail.com>
2022-10-10 00:34:03 +0530

    change item and set color

--- DOM_Task3/javascript.js
+++ DOM_Task3/javascript.js
@@ -7,6 +7,12 @@ document.getElementById("header-title").innerText="hello Sharpner";
 var title=document.getElementById("header-title");
 title.style.border="2px solid black";
 
+// third activity
+//  change item and set color
+
+var itemList=document.getElementsByClassName("list-group-item");
+itemList[1].innerHTML='<b>Mango</b>';
+itemList[1].style.background="green";
 
 
 
