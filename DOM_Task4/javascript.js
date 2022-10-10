36824898f8f12b9a4988723e33ec7f0e890b3faf
OmkarRode <rodeomkar5@gmail.com>
2022-10-10 15:32:55 +0530

    using class change background color

--- DOM_Task4/javascript.js
+++ DOM_Task4/javascript.js
@@ -1,14 +1,21 @@
 var item= document.getElementsByClassName("list-group-item");
 // console.log(item[2]);
-item[2].textContent="Mango";
-item[2].style.background="green";
+// item[2].textContent="Mango";
+// item[2].style.background="green";
 
 // set all bold in list
 // console.log(item);
-for(var i=0;i<item.length;i++)
-{
-    item[i].style.fontWeight="bold";
-    item[i].style.color="red";
-    item[i].style.fontFamily="Times New Roman"
+// for(var i=0;i<item.length;i++)
+// {
+//     item[i].style.fontWeight="bold";
+//     item[i].style.color="red";
+//     item[i].style.fontFamily="Times New Roman"
     
-}
+// }
+
+for(var i=0;i<item.length;i++)
+ {
+    item[i].style.background="grey"
+ }
+
+ 
