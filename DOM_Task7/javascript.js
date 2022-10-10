ec68a233de1126283f4276bcecc2ea23fba1c704
OmkarRode <rodeomkar5@gmail.com>
2022-10-10 18:59:33 +0530

    all query run

---
+++ DOM_Task7/javascript.js
@@ -0,0 +1,46 @@
+// var itemlist=document.querySelector('#items');
+// console.log(itemlist.parentElement);
+// // parent node
+// console.log(itemlist.parentNode);
+// // last element 
+// console.log(itemlist.lastElementChild);
+
+// //last child
+var containerLastChild=document.querySelector('.container');
+// console.log(containerLastChild.lastChild);
+
+// var createDiv=document.createElement('div');
+// createDiv.className='newdiv';
+// var createChild=document.createTextNode("hello omkar");
+// createDiv.appendChild(createChild);
+// console.log(createDiv);
+
+var itemlist=document.querySelector('#items');
+// itemlist.firstElementChild.style.background="green";
+// console.log(itemlist.nextSibling)
+// console.log(itemlist.nextElementSibling)
+// console.log(itemlist.previousSibling)
+// var title= itemlist.previousElementSibling;
+// title.style.background="red";
+
+
+// var newelement=document.createElement("div");
+// newelement.className="newdiv";
+// newelement.setAttribute('title','hello world');
+
+
+
+
+
+// var createDiv=document.createElement('div');
+// createDiv.className='newdiv';
+// var createChild=document.createTextNode("hello omkar");
+// createDiv.appendChild(createChild);
+
+
+// console.log(createDiv);
+// container.insertBefore(createDiv,header);
+
+// var container=document.querySelector('header .container');
+// var header=document.querySelector('header h1');
+
