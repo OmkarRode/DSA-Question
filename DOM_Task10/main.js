68001ce5fe756a178bc55f6e9327d3941394e177
OmkarRode <rodeomkar5@gmail.com>
2022-10-12 16:28:21 +0530

    sotore data in local sorage in obj format.

--- DOM_Task10/main.js
+++ DOM_Task10/main.js
@@ -10,7 +10,26 @@ function additem(e){
 
     var name=document.getElementById('name').value;
 var email=document.getElementById('email').value;
-localStorage.setItem(name,email);
+if(name=='' || email=='')
+{
+    alert("plaese fill information");
+}
+else
+{
+var myobj={
+    OName: name,
+    OEmail : email
+}
+
+var myobjSerilized=JSON.stringify(myobj);
+// console.log(myobjSerilized);
+localStorage.setItem(name,myobjSerilized);
+
+// desrization process ;
+var myObjDeserilized=JSON.stringify(localStorage.getItem(name));
+console.log(myObjDeserilized);
+
+// localStorage.setItem(name,email);
 alert("data is sumbmited sucessfully");
 var li =document.createElement("li");
     li.className="item";
@@ -18,4 +37,6 @@ var li =document.createElement("li");
 
 
      it.appendChild(li);
+}
+
 }
