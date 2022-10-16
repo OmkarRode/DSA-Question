0ccdb704714dd9293d8384453e084af7c30b02b6
OmkarRode <rodeomkar5@gmail.com>
2022-10-17 01:01:14 +0530

    changes regarding making edit and delete btn

--- DOM_Task10/main.js
+++ DOM_Task10/main.js
@@ -8,35 +8,48 @@ var it=document.getElementById('it');
 function additem(e){
     e.preventDefault();
 
-    var name=document.getElementById('name').value;
-var email=document.getElementById('email').value;
-if(name=='' || email=='')
+    var iname=document.getElementById('name').value;
+var iemail=document.getElementById('email').value;
+if(iname=='' || iemail=='')
 {
     alert("plaese fill information");
 }
 else
 {
 var myobj={
-    OName: name,
-    OEmail : email
+    OName: iname,
+    OEmail : iemail
 }
 
 var myobjSerilized=JSON.stringify(myobj);
 // console.log(myobjSerilized);
-localStorage.setItem(email,myobjSerilized);
+localStorage.setItem(iemail,myobjSerilized);
 
 // desrization process ;
-var myObjDeserilized=JSON.stringify(localStorage.getItem(name));
-console.log(myObjDeserilized);
+// var myObjDeserilized=JSON.stringify(localStorage.getItem(name));
+// console.log(myObjDeserilized);
 
 // localStorage.setItem(name,email);
-alert("data is sumbmited sucessfully");
+// alert("data is sumbmited sucessfully");
 var li =document.createElement("li");
     li.className="item";
-    li.appendChild(document.createTextNode(`${name} ${email}`));
-
-
+    // var btn = (`<button onclick=edit(${iname})>edit</button><button onclick=delete()>Delete</button>`);
+    var btn = (`<button>edit</button><button onclick=delete()>Delete</button>`);
+    console.log(li.childNodes[1]);
+    li.appendChild(document.createTextNode(`${iname} ${iemail}`));
+    li.innerHTML=li.innerHTML+btn;       
      it.appendChild(li);
+    // 
+    li.childNodes[1].id='iemail';
 }
 
 }
+
+
+
+//edit button
+function edit(iemail){
+   var input= document.getElementById('name');
+   if(confirm('are you')){
+input.value=iemail;}
+};
